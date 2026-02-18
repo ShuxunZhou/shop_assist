45e5fcc4f837fdbd8c76a6843259a14c0f1ca212
ShuxunZhou <980469496@qq.com>
2026-02-17 21:51:20 -0500

    删除之前假agent部分，全面改用langgraph

--- agent/mcp_server/mcp_tools.py
+++ agent/mcp_server/mcp_tools.py
@@ -1,26 +1,26 @@
 from langchain_community.utilities import SQLDatabase
 from mcp.server import FastMCP
-from agent.sql_graph.llm_model import zhipuai_client
+# from agent.sql_graph.llm_model import zhipuai_client
 
 mcp_server = FastMCP(name="shuxun-mcp", instructions='我自己的MCP服务', port=8000)
 db = SQLDatabase.from_uri(
         'mysql+pymysql://root:1234@localhost:3306/shop_assist'
     )
 
-@mcp_server.tool('my_search_tool', description='专门搜索互联网中的内容')
-def my_search(query: str) -> str:
-    """搜索互联网上的内容"""
-    try:
-        response = zhipuai_client.web_search.web_search(
-            search_engine='search-std',
-            search_query=query
-        )
-        print(response)
-        if response.search_result:
-            return "\n\n".join([d.content for d in response.search_result])
-    except Exception as e:
-        print(e)
-        return '没有搜索到任何内容！'
+# @mcp_server.tool('my_search_tool', description='专门搜索互联网中的内容')
+# def my_search(query: str) -> str:
+#     """搜索互联网上的内容"""
+#     try:
+#         response = zhipuai_client.web_search.web_search(
+#             search_engine='search-std',
+#             search_query=query
+#         )
+#         print(response)
+#         if response.search_result:
+#             return "\n\n".join([d.content for d in response.search_result])
+#     except Exception as e:
+#         print(e)
+#         return '没有搜索到任何内容！'
 
 
 
